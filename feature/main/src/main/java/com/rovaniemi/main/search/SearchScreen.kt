68484bff0a93b325f92f24121d5844df9199ab94
송현영 <150510369+rovaniemii@users.noreply.github.com>
2025-04-19 16:06:14 +0900

    refact: search screen, storage screen 프리뷰 적용

--- feature/main/src/main/java/com/rovaniemi/main/search/SearchScreen.kt
+++ feature/main/src/main/java/com/rovaniemi/main/search/SearchScreen.kt
@@ -12,15 +12,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
+import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.rovaniemi.main.compose.view.ItemsGridView
 import com.rovaniemi.main.compose.viewdata.SearchViewData
 import com.rovaniemi.main.search.view.SearchErrorView
 import com.rovaniemi.main.search.view.SearchLoadingView
 import com.rovaniemi.main.search.view.SearchWordInputView
+import kotlinx.coroutines.flow.flowOf
 
 @Composable
 internal fun SearchScreen(
@@ -78,4 +82,41 @@ internal fun SearchScreen(
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun PreviewSearchScreen() {
+    fun fakeItems(): List<SearchViewData> = listOf(
+        SearchViewData(
+            id = 1L,
+            thumbnail = "",
+            dateTime = "",
+            isBookmark = true,
+        ),
+        SearchViewData(
+            id = 1L,
+            thumbnail = "",
+            dateTime = "",
+            isBookmark = false,
+        ),
+        SearchViewData(
+            id = 1L,
+            thumbnail = "",
+            dateTime = "",
+            isBookmark = false,
+        ),
+    )
+
+    val fakePagingData = flowOf(PagingData.from(fakeItems()))
+    val lazyItems = fakePagingData.collectAsLazyPagingItems()
+
+    SearchScreen(
+        navigationHeight = 0.dp,
+        isSearchInitialized = true,
+        query = "부산",
+        items = lazyItems,
+        onSearchButtonClick = {},
+        onClickUpdateBookmark = {}
+    )
 }
--- feature/main/src/main/java/com/rovaniemi/main/storage/StorageScreen.kt
+++ feature/main/src/main/java/com/rovaniemi/main/storage/StorageScreen.kt
@@ -2,10 +2,15 @@ package com.rovaniemi.main.storage
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.rovaniemi.main.compose.view.ItemsGridView
 import com.rovaniemi.main.compose.viewdata.SearchViewData
+import kotlinx.coroutines.flow.flowOf
 
 @Composable
 internal fun StorageScreen(
@@ -21,5 +26,39 @@ internal fun StorageScreen(
         onClickItem = {
             deleteItem(it.id)
         }
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewStorageScreen() {
+    fun fakeItems(): List<SearchViewData> = listOf(
+        SearchViewData(
+            id = 1L,
+            thumbnail = "",
+            dateTime = "",
+            isBookmark = true,
+        ),
+        SearchViewData(
+            id = 1L,
+            thumbnail = "",
+            dateTime = "",
+            isBookmark = false,
+        ),
+        SearchViewData(
+            id = 1L,
+            thumbnail = "",
+            dateTime = "",
+            isBookmark = false,
+        ),
+    )
+
+    val fakePagingData = flowOf(PagingData.from(fakeItems()))
+    val lazyItems = fakePagingData.collectAsLazyPagingItems()
+
+    StorageScreen(
+        navigationHeight = 0.dp,
+        items = lazyItems,
+        deleteItem = {}
     )
 }
